dea171673f316c511877cb9fd708f47d75077e6b
luca-virninch <93200970+luca-virninch@users.noreply.github.com>
2022-04-26 00:16:20 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,37 +2,59 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let player_left = document.getElementById('player_left')
     let yPos = 0
+    let isMovingUp 
 
     function control (e) {
-        
 
+        
+        
+        //MOVE UP
         if (e.keyCode===87){
             console.log("UP")
-
-            //MOVE UP
+            
+            isMovingUp = true
+            console.log(isMovingUp)
+            
+            window.requestAnimationFrame(LeftMovementUP)
+        
         }
 
+        //MOVE DOWN
         if (e.keyCode===83){
             console.log("DOWN")
 
-            //MOVE DOWN
+            isMovingUp = false
+            console.log(isMovingUp)
+
+            window.requestAnimationFrame(LeftMovementDown)            
         }
 
         
     }
     
-    document.addEventListener('keydown', control)
+    document.addEventListener('keyup', control)
 
 
 
-    function LeftMovement (){
-        yPos = yPos - 5
+    function LeftMovementUP (){
+        
+        yPos = yPos - 3
         player_left.style.transform = `translateY(${yPos}px)`;
 
-        requestAnimationFrame(LeftMovement)
+       requestAnimationFrame(LeftMovementUP)
+        
     }
 
-    window.requestAnimationFrame(LeftMovement)
+    function LeftMovementDown (){
+        
+        yPos = yPos + 3
+        player_left.style.transform = `translateY(${yPos}px)`;
+
+        requestAnimationFrame(LeftMovementDown)
+        
+    }
+
+   
 
     
 
@@ -41,4 +63,5 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 })
+
 
